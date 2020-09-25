fc5c391c63f99fc83f7867e5d773b0244f3af810
Brandon Cuthbertson <57675300+BrandonCuthbertson@users.noreply.github.com>
2020-09-25 11:15:27 -0600

    Brah

---
+++ JS/database.js
@@ -0,0 +1,8 @@
+//Home Page array
+var cardHeader = ["", "Two"];
+var cardIcon = ["A", "B"]
+var cardTitle = ["One", "Two"];
+var cardText = ["One", "Two"];
+var cardLink = ["#", "#"];
+var cardLinkText = ["one", "two"];
+var cardLen = cardHeader.length;
