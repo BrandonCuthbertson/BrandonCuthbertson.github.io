219c84294f32f26515ab934ce5c4a6ea0eeda22f
Brandon Cuthbertson <57675300+BrandonCuthbertson@users.noreply.github.com>
2020-10-13 15:02:51 -0600

    Resume + Tab Update
    Update Resume page and tabs now save location based on session

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,17 +1,26 @@
+var tabName;
 // On load hides all tabs and displays About me
-//Work around because default id is not working
-tabcontent = document.getElementsByClassName("tabcontent");
-for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+if (sessionStorage.current != null) {
+   
+    removeTabs();
+    tabName = sessionStorage.getItem("current");
+    document.getElementById(tabName).style.display = "block";
+    document.getElementById('nav' + tabName).className += " active";
+    document.getElementById('Top-' + tabName).className += " active";
+
 }
-document.getElementById('About').style.display = "block";
-evt.currentTarget.className += " active";
+else {
+    tabName = 'About';
+document.getElementById(tabName).style.display = "block";
+document.getElementById('nav' + tabName).className += " active";
+    document.getElementById('Top-' + tabName).className += " active";
+}
 
 
-function openPage(evt, pageName) {
+function removeTabs(){
     // Declare all variables
     var i, tabcontent, tablinks;
-    
+
 
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -24,8 +33,13 @@ function openPage(evt, pageName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+}
+
+function openPage(evt, pageName) {
+    removeTabs();
 
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(pageName).style.display = "block";
     evt.currentTarget.className += " active";
+    sessionStorage.setItem("current", pageName);
 }
