c13bac7d0210e13062929410633df94a3c03cdc1
Brandon Cuthbertson <57675300+BrandonCuthbertson@users.noreply.github.com>
2020-10-28 14:27:10 -0600

    Main Page almost complete

--- JS/database.js
+++ JS/database.js
@@ -6,3 +6,9 @@ var cardText = ["One", "Two"];
 var cardLink = ["#", "#"];
 var cardLinkText = ["one", "two"];
 var cardLen = cardHeader.length;
+
+var cardDB = [
+    Website = { Icons: "Fiat", Photo: "500", Title: "white", Desc: "Woooooooords", Link: "test", Tag: "Fort End" },
+    Empty = { Icons: "<i class='fas fa- grin'></i>", Photo: "500", Title: "white", Desc: "Woooooooords", Link: "test", Tag: "Fort End" },
+];
+var cardDBLength = cardDB.length;
