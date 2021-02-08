5dbe8d498afb2ea2592677b1a432bd9835255382
Brandon Cuthbertson <57675300+BrandonCuthbertson@users.noreply.github.com>
2021-02-08 09:13:33 -0700

    Added Project

--- JS/database.js
+++ JS/database.js
@@ -1,7 +1,8 @@
 
 var cardDB = [
     Template = { Picture: "template", Title: "Template", Desc: "A Quick Summary of the Project and what im working on", Link: "#", Tag: "" },
-    Website = { Picture: "website", Title: "Personal Website", Desc: "A Fully Responsive Webpage built using HTML5, CSS3, JavaScript, and BootStrap", Link: "https://github.com/BrandonCuthbertson/BrandonCuthbertson.github.io", Tag: "Front-End Back-End" }
+    Website = { Picture: "website", Title: "Personal Website", Desc: "A Fully Responsive Webpage built using HTML5, CSS3, JavaScript, and BootStrap", Link: "https://github.com/BrandonCuthbertson/BrandonCuthbertson.github.io", Tag: "Front-End Back-End" },
+    Template = { Picture: "ftfscraper", Title: "Personal Scraper", Desc: "A Personal Web Scraper using Discord.py to get prices for cards whenever someone enters <code>$$CardName$$</code>.", Link: "https://repl.it/@BCuthbertson/FTF-Discord#main.py", Tag: "Front-End Back-End Mobile-App" },
 ];
 /*TAGS ARE
  * Front-End
